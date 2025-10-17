f53a0901d50d1189ab274a2515a76dbe8d87b90b
TimKuo0927 <tim.kuo.yt@gmail.com>
2025-10-16 21:33:38 -0400

    both: finish create eposide and detail

---
+++ group#13(YenTing&Favour)_Lab#3/Controllers/EpisodeController.cs
@@ -0,0 +1,109 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using group_13_YenTing_Favour__Lab_3.Models;
+using group_13_YenTing_Favour__Lab_3.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace group_13_YenTing_Favour__Lab_3.Controllers
+{
+    public class EpisodeController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly Lab3Context _db;
+        public EpisodeController(UserManager<IdentityUser> userManager, Lab3Context db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult CreateEposidePage(int PodcastId)
+        {
+
+            var userIdString = _userManager.GetUserId(User);
+            if (String.IsNullOrEmpty(userIdString))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            Episode episode = new Episode();
+            episode.PodcastId = PodcastId;
+            return View(episode);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateEpisode(Episode episode, IFormFile VideoFile)
+        {
+            Console.WriteLine("PodcastId: " + episode.PodcastId);
+            if (VideoFile == null || VideoFile.Length == 0)
+            {
+                ModelState.AddModelError("", "Please provide a video file.");
+                return View(episode);
+            }
+
+            // 1. Get the username
+            var userName = User.Identity?.Name ?? "unknown";
+
+            // 2. Generate a unique filename
+            var fileExtension = Path.GetExtension(VideoFile.FileName);
+            var fileKey = $"{userName}/{Guid.NewGuid()}{fileExtension}";
+
+            // 3. Upload to S3
+            using (var stream = VideoFile.OpenReadStream())
+            {
+                var putRequest = new Amazon.S3.Model.PutObjectRequest
+                {
+                    BucketName = AWSUtil.bucketName,
+                    Key = fileKey,
+                    InputStream = stream,
+                    ContentType = VideoFile.ContentType,
+                };
+
+                await AWSUtil.s3Client.PutObjectAsync(putRequest);
+            }
+
+            // 4. Save Episode info to DB
+            episode.AudioFileUrl = fileKey;
+            episode.PlayCount = 0;
+            episode.ReleaseDate = DateTime.UtcNow;
+            episode.Duration = VideoFile.Length; // assuming duration is in bytes for simplicity
+            episode.NumberOfViews = 0;
+
+            await _db.Episodes.AddAsync(episode);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("PodcastDetailPage", "Podcast",new { PodcastId = episode.PodcastId }); 
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> EposideDetailPage(int EpisodeId)
+        {
+
+            var userIdString = _userManager.GetUserId(User);
+            if (String.IsNullOrEmpty(userIdString))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            var episode = await _db.Episodes
+               .Include(e => e.Podcast)
+               .FirstOrDefaultAsync(e => e.EpisodeId == EpisodeId);
+
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = AWSUtil.bucketName,
+                Key = episode.AudioFileUrl,
+                Expires = DateTime.UtcNow.AddHours(1)
+            };
+            var url = AWSUtil.s3Client.GetPreSignedURL(request);
+            episode.AudioFileUrl = url;
+            return View(episode);
+        }
+
+    }
+}
---
+++ group#13(YenTing&Favour)_Lab#3/Services/AWSUtil.cs
@@ -0,0 +1,51 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.S3;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace group_13_YenTing_Favour__Lab_3.Services
+{
+    public class AWSUtil
+    {
+        public readonly static IAmazonS3 s3Client;
+        private static readonly IConfiguration _config;
+        public readonly static string bucketName;
+        //public readonly static AmazonDynamoDBClient dynamoClient;
+        //public readonly static Table userTable;
+
+        // static constructor runs once, the first time any member of Helper is accessed
+        static AWSUtil()
+        {
+            var builder = new ConfigurationBuilder()
+                 .SetBasePath(AppContext.BaseDirectory)
+                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            _config = builder.Build();
+            s3Client = GetS3Client();
+            bucketName = _config["AWS:BucketName"];
+            //dynamoClient = GetDynamoClient();
+            //userTable = LoadUserTable("Bookshelf"); // <-- change to your DynamoDB table name
+        }
+
+        private static IAmazonS3 GetS3Client()
+        {
+            var accessId = _config["AWS:AccessId"];
+            var secretKey = _config["AWS:SecretKey"];
+            var region = _config["AWS:Region"];
+
+            return new AmazonS3Client(accessId, secretKey, Amazon.RegionEndpoint.GetBySystemName(region));
+        }
+
+        //private static AmazonDynamoDBClient GetDynamoClient()
+        //{
+        //    var credentials = new BasicAWSCredentials(
+        //        ConfigurationManager.AppSettings["accessId"],
+        //        ConfigurationManager.AppSettings["secretKey"]);
+        //    return new AmazonDynamoDBClient(credentials, RegionEndpoint.USEast1);
+        //}
+
+        //private static Table LoadUserTable(string tableName)
+        //{
+        //    // loads metadata for the given DynamoDB table
+        //    return Table.LoadTable(dynamoClient, tableName, DynamoDBEntryConversion.V2);
+        //}
+    }
+}
